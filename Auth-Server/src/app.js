4c404cbaccedc1091ac2ae0d82de87facf51cea7
Matt Stockton <mattjezstockton@gmail.com>
2021-01-30 14:52:46 -0800

    Added oidc callback

--- Auth-Server/src/app.js
+++ Auth-Server/src/app.js
@@ -4,11 +4,37 @@ const	app	= express();
 
 const Provider = require('oidc-provider');
 
+const configuration = {
+    claims: {
+        email: ['email', 'email_verified'],
+        phone: ['phone_number', 'phone_number_verified'],
+        profile: ['birthdate', 'family_name', 'gender', 'given_name', 'locale', 'middle_name', 'name', 'nickname', 'picture', 'preferred_username', 'profile', 'updated_at', 'website', 'zoneinfo']
+    },
+    clients: [{
+        client_id: 'test_implicit_app',
+        grant_types: ['implicit'],
+        response_types: ['id_token'],
+        redirect_uris: ['https://alphanetrics.com/redirect'],
+        // + other client properties
+    }],
+    scopes: ['api'],
+    async findById(ctx, id) {
+        return {
+            accountId: id,
+            async claims() { return { sub: id }; },
+        };
+    }
+}
+
+const oidc = new Provider('http://localhost:3001', configuration);
+
 app.use(express.static(path.join(__dirname, '..', '..', 'sample-app', 'build')));
 
 app.get('/test', (req, res) => {
     res.send({message: 'This is a test change'})
-})
+});
+
+app.use('/auth', oidc.callback);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'sample-app', 'build', 'index.html'));
--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useRouteMatch } from "react-router-dom";
 
-function App() {
+const App = () => {
+  let match = useRouteMatch('/redirect')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(e)=> startAuth(e)}>Login</button>
+      {match ? <AuthCallback match={match} /> : <div />}
     </div>
   );
+}
+
+const startAuth = (event) => {
+  axios.get('http://192.168.0.26/auth/auth', {
+    params: {
+      client_id: 'test_implicit_app',
+      redirect_uri: 'https://alphanetrics.com/redirect',
+      response_type: 'id_token',
+      scope: 'openid profile',
+      nonce: '123',
+      state: '321'
+    }
+  })
+  // .then((res) => {
+  //   console.log(res)
+  //   window.location.href= res.data.url
+
+  // })
+}
+
+const AuthCallback = (props) => {
+  // let url = window.location.href
+  // let urlParams = new URLSearchParams(url)
+  // let questionMark = url.indexOf("=");
+  // let lastIndex = url.indexOf("&")
+  // let code = url.slice(questionMark + 1, lastIndex)
+  // let state = urlParams.get('state')
+  // let realmId = urlParams.get('realmId');
+  // // let officeId = url.get('realmId');
+  // console.log(window.location.href)
+  // console.log(code)
+  // console.log(realmId)
+  // axios.get('http://localhost:3001/qbo/callback' + "?code=" + code + "&realmId=" + realmId + "&officeId=1"  ).then((res) => {
+
+  // })
+  return <div>Nice</div>
 }
 
 export default App;
